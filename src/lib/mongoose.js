f99e99e0dc7718f1bb4cb739941877513b37fc7d
Ken Ashly Cabrales <kencabrales4@gmail.com>
2026-04-28 16:32:11 +0800

    fix: replace Proces singInstructions.env with process.env

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const MONGODB_URI = 
-ProcessingInstruction.env.MONGODB_URI;
+process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
     throw new Error ('Please define the MONGO_URI environment variable inside .env.local');
